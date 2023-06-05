4c9ae4009ce3861996901370436bb24c229e5a3f
Vincent Liussyaputra <liusvinv@gmail.com>
2023-06-06 05:09:37 +0700

    Add webhooks responder

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.post('/webhook', async (req, res) => {
   // Use Apify to fetch data or perform scraping
-  // and send the response back to the client
-//   const data = await fetchData();
-    console.log(req);
-    res.status(200).send();
+    const payload = req.body;
+    console.log(payload);
+    console.log('haize');
+    res.sendStatus(200);
 });
 
 // Define other routes as needed
